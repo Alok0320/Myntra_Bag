9aefeb12303b2ee6e699952182a0d2b92ade821b
Alok0320 <alokupadhyay0188@gmail.com>
2024-12-02 20:33:22 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,10 @@
+let cartQuantity = 0;
+document.querySelector('#cart-summary').innerText =`Your Bag has ${cartQuantity} items`;
+
+/* Code for displaying name on web page
+const myName = 'Alok';
+document.querySelector('#name-display').
+innerText = myName;
+*/
+
+// console.log(eval('45 + 90'));  eval method make any string consider as mathmatical expression 
